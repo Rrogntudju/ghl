040d31ff53d650f3e90b238412034716231e5f9d
rrogntudju <rrogntudju@doh>
2022-06-03 17:50:51 -0400

    ménage

--- crates/ghl/src/main.rs
+++ crates/ghl/src/main.rs
@@ -1,6 +1,5 @@
-use regex::Regex;
+use regex::{escape, Regex};
 use reqwest::blocking::Client;
-use rulex::Rulex;
 use soup::prelude::*;
 use std::env::args;
 use std::error::Error;
@@ -16,7 +15,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     };
     let page = Client::new().get(url_latest).send()?.text()?;
     let soup = Soup::new(&page);
-    let regex = Rulex::parse_and_compile(&format!(r"<% [.]* '/{download}' %>"), Default::default())?;
+    let regex = format!(r"^.+/{}$", escape(&download));
     match soup.tag("a").attr("href", Regex::new(&regex)?).find() {
         Some(tag) => println!("https://github.com{}", tag.get("href").unwrap()),
         None => return Err("Fichier à télécharger introuvable".into()),
