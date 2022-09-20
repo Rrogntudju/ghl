34399d3a5465292400df8bda23688aa52c04b46b
rrogntudju <you@example.com>
2022-09-19 20:56:12 -0400

    version 0.2

--- crates/ghl/src/main.rs
+++ crates/ghl/src/main.rs
@@ -1,4 +1,4 @@
-use regex::{escape, Regex};
+use regex::Regex;
 use reqwest::blocking::Client;
 use soup::prelude::*;
 use std::env::args;
@@ -9,16 +9,18 @@ fn main() -> Result<(), Box<dyn Error>> {
         Some(url) => Url::parse(&url)?,
         None => return Err("Url «latest» manquant".into()),
     };
-    let download = match args().nth(2) {
-        Some(download) => download,
-        None => return Err("Fichier à télécharger manquant".into()),
-    };
     let page = Client::new().get(url_latest).send()?.text()?;
     let soup = Soup::new(&page);
-    let regex = format!(r"^.+/{}$", escape(&download));
-    match soup.tag("a").attr("href", Regex::new(&regex)?).find() {
-        Some(tag) => println!("https://github.com{}", tag.get("href").unwrap()),
-        None => return Err("Fichier à télécharger introuvable".into()),
-    };
-    Ok(())
+    let regex = Regex::new(r"^Release (.*) · cloudflare/cloudflared · GitHub$")?;
+    match soup.tag("title").find() {
+        Some(titre) => {
+            if let Some(captures) = regex.captures(&titre.text()) {
+                println!("{}", captures.get(1).unwrap().as_str());
+                Ok(())
+            } else {
+                Err("version introuvable".into())
+            }
+        }
+        None => Err("titre introuvable".into()),
+    }
 }
