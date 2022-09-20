6228c2f209821a096a5e832cdc0efb54d34de24e
rrogntudju <you@example.com>
2022-09-20 10:49:14 -0400

    nit

--- crates/ghl/src/main.rs
+++ crates/ghl/src/main.rs
@@ -5,22 +5,22 @@ use std::env::args;
 use std::error::Error;
 use url::Url;
 fn main() -> Result<(), Box<dyn Error>> {
-    let url_latest = match args().nth(1) {
-        Some(url) => Url::parse(&url)?,
-        None => return Err("Url «latest» manquant".into()),
-    };
-    let page = Client::new().get(url_latest).send()?.text()?;
-    let soup = Soup::new(&page);
-    match soup.tag("title").find() {
-        Some(titre) => {
-            let regex = Regex::new(r"^Release (.*) · cloudflare/cloudflared · GitHub$")?;
-            if let Some(captures) = regex.captures(&titre.text()) {
-                println!("{}", captures.get(1).unwrap().as_str());
-                Ok(())
-            } else {
-                Err("version introuvable".into())
+    if let Some(url) = args().nth(1) {
+        let page = Client::new().get(Url::parse(&url)?).send()?.text()?;
+        let soup = Soup::new(&page);
+        match soup.tag("title").find() {
+            Some(titre) => {
+                let regex = Regex::new(r"^Release (.*) · cloudflare/cloudflared · GitHub$")?;
+                if let Some(captures) = regex.captures(&titre.text()) {
+                    println!("{}", captures.get(1).unwrap().as_str());
+                    Ok(())
+                } else {
+                    Err("version introuvable".into())
+                }
             }
+            None => Err("titre introuvable".into()),
         }
-        None => Err("titre introuvable".into()),
+    } else {
+        Err("Url «latest» manquant".into())
     }
 }
